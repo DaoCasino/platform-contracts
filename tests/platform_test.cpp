494d2d203a90091dfea19f9c214ad33e48949a3b
Algys Ievlev <algys.ievlev@gmail.com>
2020-03-05 10:27:38 +0300

    [platform] add game actions tests

--- tests/platform_test.cpp
+++ tests/platform_test.cpp
@@ -184,7 +184,7 @@ BOOST_FIXTURE_TEST_CASE(casino_delete_test, platform_tester) try {
 
 } FC_LOG_AND_RETHROW()
 
-BOOST_FIXTURE_TEST_CASE(auto_increment_test, platform_tester) try {
+BOOST_FIXTURE_TEST_CASE(casino_id_auto_increment_test, platform_tester) try {
     std::vector<account_name> casinos = {
         N(casino.1),
         N(casino.2),
@@ -230,6 +230,204 @@ BOOST_FIXTURE_TEST_CASE(auto_increment_test, platform_tester) try {
 
 } FC_LOG_AND_RETHROW()
 
+
+BOOST_FIXTURE_TEST_CASE(add_game, platform_tester) try {
+    account_name game_account = N(game.1);
+
+    create_account(game_account);
+
+    base_tester::push_action(platform_name, N(addgame), platform_name, mvo()
+        ("contract", game_account)
+        ("params_cnt", 0)
+        ("meta", bytes())
+    );
+
+    auto game = get_game(0);
+    BOOST_REQUIRE(!game.is_null());
+
+    BOOST_REQUIRE_EQUAL(game["contract"].as<account_name>(), game_account);
+    BOOST_REQUIRE_EQUAL(game["meta"].as<bytes>().size(), 0);
+    BOOST_REQUIRE_EQUAL(game["params_cnt"].as<uint64_t>(), 0);
+    BOOST_REQUIRE_EQUAL(game["paused"].as<bool>(), false);
+
+} FC_LOG_AND_RETHROW()
+
+BOOST_FIXTURE_TEST_CASE(add_many_games, platform_tester) try {
+    std::vector<account_name> games = {
+        N(game.1),
+        N(game.2),
+        N(game.3),
+        N(game.4),
+        N(game.5),
+    };
+
+    for (const auto& game: games) {
+        create_account(game);
+
+        base_tester::push_action(platform_name, N(addgame), platform_name, mvo()
+            ("contract", game)
+            ("params_cnt", 0)
+            ("meta", bytes())
+        );
+    }
+
+    for (auto i = 0; i < games.size(); ++i) {
+        auto game = get_game(i);
+        BOOST_REQUIRE(!game.is_null());
+
+        BOOST_REQUIRE_EQUAL(game["contract"].as<account_name>(), games[i]);
+        BOOST_REQUIRE_EQUAL(game["meta"].as<bytes>().size(), 0);
+        BOOST_REQUIRE_EQUAL(game["paused"].as<bool>(), false);
+    }
+
+} FC_LOG_AND_RETHROW()
+
+BOOST_FIXTURE_TEST_CASE(set_pause_game, platform_tester) try {
+    account_name game_account = N(game.1);
+
+    create_account(game_account);
+
+    base_tester::push_action(platform_name, N(addgame), platform_name, mvo()
+        ("contract", game_account)
+        ("params_cnt", 0)
+        ("meta", bytes())
+    );
+
+    base_tester::push_action(platform_name, N(pausegame), platform_name, mvo()
+        ("id", 0)
+        ("pause", true)
+    );
+
+    auto game = get_game(0);
+
+    BOOST_REQUIRE_EQUAL(game["paused"].as<bool>(), true);
+
+    base_tester::push_action(platform_name, N(pausegame), platform_name, mvo()
+        ("id", 0)
+        ("pause", false)
+    );
+
+    game = get_game(0);
+
+    BOOST_REQUIRE_EQUAL(game["paused"].as<bool>(), false);
+} FC_LOG_AND_RETHROW()
+
+BOOST_FIXTURE_TEST_CASE(set_contract_game, platform_tester) try {
+    account_name game_account = N(game.1);
+    account_name game_account_new = N(game.2);
+
+    create_account(game_account);
+    create_account(game_account_new);
+
+    base_tester::push_action(platform_name, N(addgame), platform_name, mvo()
+        ("contract", game_account)
+        ("params_cnt", 0)
+        ("meta", bytes())
+    );
+
+    base_tester::push_action(platform_name, N(setcontrgame), platform_name, mvo()
+        ("id", 0)
+        ("contract", game_account_new)
+    );
+
+    auto game = get_game(0);
+
+    BOOST_REQUIRE_EQUAL(game["contract"].as<account_name>(), game_account_new);
+
+} FC_LOG_AND_RETHROW()
+
+BOOST_FIXTURE_TEST_CASE(set_meta_game, platform_tester) try {
+    account_name game_account = N(game.1);
+
+    create_account(game_account);
+
+    base_tester::push_action(platform_name, N(addgame), platform_name, mvo()
+        ("contract", game_account)
+        ("params_cnt", 0)
+        ("meta", bytes())
+    );
+
+    bytes new_meta { 'n', 'e', 'w', ' ', 'm', 'e', 't', 'a' };
+
+    base_tester::push_action(platform_name, N(setmetagame), platform_name, mvo()
+        ("id", 0)
+        ("meta", new_meta)
+    );
+
+    auto game = get_game(0);
+
+    BOOST_TEST(game["meta"].as<bytes>() == new_meta);
+
+} FC_LOG_AND_RETHROW()
+
+BOOST_FIXTURE_TEST_CASE(game_delete_test, platform_tester) try {
+    account_name game_account = N(game.1);
+
+    create_account(game_account);
+
+    base_tester::push_action(platform_name, N(addgame), platform_name, mvo()
+        ("contract", game_account)
+        ("params_cnt", 0)
+        ("meta", bytes())
+    );
+
+    base_tester::push_action(platform_name, N(delgame), platform_name, mvo()
+        ("id", 0)
+    );
+
+    auto game = get_game(0);
+
+    BOOST_REQUIRE_EQUAL(game.is_null(), true);
+
+} FC_LOG_AND_RETHROW()
+
+BOOST_FIXTURE_TEST_CASE(game_id_auto_increment_test, platform_tester) try {
+    std::vector<account_name> games = {
+        N(game.1),
+        N(game.2),
+        N(game.3),
+        N(game.4),
+        N(game.5),
+    };
+
+    for (auto i = 0; i < 3; ++i) {
+        create_account(games[i]);
+
+        base_tester::push_action(platform_name, N(addgame), platform_name, mvo()
+            ("contract", games[i])
+            ("params_cnt", 0)
+            ("meta", bytes())
+        );
+
+        produce_blocks(2);
+
+        auto game = get_game(i);
+        BOOST_REQUIRE_EQUAL(game["id"].as<uint64_t>(), i);
+    }
+
+    for (auto i = 0; i < 3; ++i) {
+        base_tester::push_action(platform_name, N(delgame), platform_name, mvo()
+            ("id", i)
+        );
+    }
+
+    for (auto i = 3; i < 5; ++i) {
+        create_account(games[i]);
+
+        base_tester::push_action(platform_name, N(addgame), platform_name, mvo()
+            ("contract", games[i])
+            ("params_cnt", 0)
+            ("meta", bytes())
+        );
+
+        produce_blocks(2);
+
+        auto game = get_game(i);
+        BOOST_REQUIRE_EQUAL(game.is_null(), false);
+        BOOST_REQUIRE_EQUAL(game["id"].as<uint64_t>(), i);
+    }
+
+} FC_LOG_AND_RETHROW()
 
 BOOST_AUTO_TEST_SUITE_END()
 } // namespace testing
